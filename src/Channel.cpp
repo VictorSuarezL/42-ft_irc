953109d261add2d66643eb9180db067627f2ced0
vsanz-su <vsslledo@gmail.com>
2026-07-24 17:44:30 +0200

    Mejora la impresión del límite de usuarios en la información del canal

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -145,7 +145,12 @@ void Channel::printChannelInfo() const {
     std::cout << "Invite Only: " << (_inviteOnly ? "Yes" : "No") << std::endl;
     std::cout << "Topic Restricted: " << (_topicRestricted ? "Yes" : "No") << std::endl;
     std::cout << "Channel Key: " << (_channelKey.empty() ? "None" : _channelKey) << std::endl;
-    std::cout << "User Limit: " << (_userLimit > 0 ? std::to_string(_userLimit) : "None") << std::endl;
+    std::cout << "User Limit: ";
+    if (_userLimit > 0)
+        std::cout << _userLimit;
+    else
+        std::cout << "None";
+    std::cout << std::endl;
     std::cout << std::endl;
 }
 
